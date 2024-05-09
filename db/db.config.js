b4d22693d9415f562acedd208db98e587b1ee7c9
EngAbdullaahiMascuud <abdullaahimascuud123@gmail.com>
2024-05-09 15:26:07 +0300

    connection

--- db/db.config.js
+++ db/db.config.js
@@ -3,13 +3,19 @@
 // u isticmashid mysql, sameey database _name oo u bixi (fast_food_db)
 const mysql= require('mysql2');
 
-const connection= mysql.createConnection({
-    host: "localhost",
-    user: "root",  // if you are using custom username use your own custom username here
-    password: "", // if your are using custom password use your own custom password here
-    database: "fast_food_db"   // make sure to create this database
-})
+// const connection= mysql.createConnection({
+//     host: "localhost",
+//     user: "root",  // if you are using custom username use your own custom username here
+//     password: "", // if your are using custom password use your own custom password here
+//     database: "fast_food_db"   // make sure to create this database
+// })
 
+const connection= mysql.createConnection({
+        host: "localhost",
+        user: "id22143724_electronicsdb001",  // if you are using custom username use your own custom username here
+        password: "Adan-electronics01", // if your are using custom password use your own custom password here
+        database: "id22143724_electronicsdb"   // make sure to create this database
+    })
 connection.connect((err)=>{
     if(err) throw err;
 
